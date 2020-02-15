6fd9fe39618445389ecfbbe88dea6fe975e79e6a
Yigitcan Alanyali <y.alanyali@gmail.com>
2020-02-15 22:35:10 +0100

    Revert "initial commit"
    This reverts commit 9fd8996508861f579a326cfc559417c79c48160c.

--- background.js
+++ background.js
@@ -1,20 +1,9 @@
-const inject = () => {
-  chrome.tabs.executeScript(
-    { file: 'script.js', allFrames: true },
-    () => chrome.runtime.lastError
-  )
+const inject = () => { chrome.tabs.executeScript({ file: 'script.js', allFrames: true }, () => chrome.runtime.lastError) }
+
+if (!document.pictureInPictureEnabled) {
+  chrome.browserAction.setTitle({ title: 'Picture-in-Picture NOT supported' })
+} else {
+  chrome.browserAction.onClicked.addListener(tab => {
+    inject()
+  })
 }
-
-chrome.browserAction.onClicked.addListener(tab => {
-  inject()
-})
-
-// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-//   if (msg.action === 'updateIcon') {
-//     if (msg.processing) {
-//       chrome.browserAction.setIcon({ path: '/assets/on.png' })
-//     } else {
-//       chrome.browserAction.setIcon({ path: '/assets/off.png' })
-//     }
-//   }
-// })
--- script.js
+++ script.js
@@ -1,61 +1,25 @@
-if (!document._audioCompressor) {
-  document._audioCompressor = {}
-}
-
 (async () => {
   const videos = [...document.querySelectorAll('video')]
     .filter(video => video.readyState !== 0)
+    .filter(video => video.disablePictureInPicture === false)
     .sort((v1, v2) => {
       const v1Rect = v1.getClientRects()[0]
       const v2Rect = v2.getClientRects()[0]
       return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height))
     })
 
-  let mediaEl = null
-  if (videos.length === 0) {
-    const audioElements = [...document.querySelectorAll('audio')].filter(audio => audio.muted === true)
-    mediaEl = audioElements[0]
+  if (videos.length === 0) { return }
+
+  const video = videos[0]
+
+  if (video.hasAttribute('__pip__')) {
+    await document.exitPictureInPicture()
   } else {
-    mediaEl = videos[0]
-  }
-
-  if (!mediaEl) { return }
-
-  const cx = document._audioCompressor
-
-  if (mediaEl.hasAttribute('__compressor_active__')) {
-    cx.compressor.ratio.setValueAtTime(1, cx.audioCtx.currentTime)
-    // chrome.runtime.sendMessage({
-    //   action: 'updateIcon',
-    //   processing: false
-    // })
-    mediaEl.removeAttribute('__compressor_active__')
-  } else {
-    if (!cx.hasOwnProperty('compressor')) {
-      // First run in tab
-      cx.audioCtx = new AudioContext()
-      cx.source = cx.audioCtx.createMediaElementSource(mediaEl)
-      cx.compressor = cx.audioCtx.createDynamicsCompressor()
-      cx.source.connect(cx.compressor)
-      cx.compressor.connect(cx.audioCtx.destination)
-
-      // Listener for setting changes
-      // TODO
-    }
-
-    const now = cx.audioCtx.currentTime
-
-    cx.compressor.threshold.setValueAtTime(-50, now)
-    cx.compressor.knee.setValueAtTime(40, now)
-    cx.compressor.ratio.setValueAtTime(20, now) // max: 20
-    cx.compressor.attack.setValueAtTime(0, now)
-    cx.compressor.release.setValueAtTime(0.15, now)
-    mediaEl.setAttribute('__compressor_active__', true)
-    // chrome.runtime.sendMessage({
-    //   action: 'updateIcon',
-    //   processing: true
-    // })
+    await video.requestPictureInPicture()
+    video.setAttribute('__pip__', true)
+    video.addEventListener('leavepictureinpicture', ev => {
+      video.removeAttribute('__pip__')
+    }, { once: true })
+    chrome.runtime.sendMessage({ message: 'enter' })
   }
 })()
-
-// document.querySelector('[__compressor_active__=true]')
