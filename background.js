9fd8996508861f579a326cfc559417c79c48160c
Yigitcan Alanyali <y.alanyali@gmail.com>
2020-02-15 22:34:02 +0100

    initial commit

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
-const inject = () => { chrome.tabs.executeScript({ file: 'script.js', allFrames: true }, () => chrome.runtime.lastError) }
-
-if (!document.pictureInPictureEnabled) {
-  chrome.browserAction.setTitle({ title: 'Picture-in-Picture NOT supported' })
-} else {
-  chrome.browserAction.onClicked.addListener(tab => {
-    inject()
-  })
+const inject = () => {
+  chrome.tabs.executeScript(
+    { file: 'script.js', allFrames: true },
+    () => chrome.runtime.lastError
+  )
 }
+
+chrome.browserAction.onClicked.addListener(tab => {
+  inject()
+})
+
+// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+//   if (msg.action === 'updateIcon') {
+//     if (msg.processing) {
+//       chrome.browserAction.setIcon({ path: '/assets/on.png' })
+//     } else {
+//       chrome.browserAction.setIcon({ path: '/assets/off.png' })
+//     }
+//   }
+// })
